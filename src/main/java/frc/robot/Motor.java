87efa11936ece088e480ca6e8f8a98a28f0bfcbe
RJ Richards <thegreenbomb3999@gmail.com>
2020-01-08 20:23:11 -0500

    Added motor type wrapper

--- src/main/java/frc/robot/Motor.java
+++ src/main/java/frc/robot/Motor.java
@@ -9,12 +9,21 @@ public class Motor {
     private PWMTalonSRX pwm;
     private CANSparkMax can;
 
+    public enum Type {
+        Brushed(0), Brushless(1);
+
+        private int id;
+        private Type(int val) {
+            id = val;
+        }
+    }
+
     public Motor(int channel) {
         pwm = new PWMTalonSRX(channel);
     }
 
-    public Motor(int channel, MotorType type) {
-        can = new CANSparkMax(channel, type);
+    public Motor(int channel, Type type) {
+        can = new CANSparkMax(channel, MotorType.fromId(type.id));
     }
 
     public PWMTalonSRX PWM() { return pwm; }
